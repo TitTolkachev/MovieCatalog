9a6eebd8f87a70d65dfd0e0700814b1fca8c7434
Тит Game <tit.tolk07022003@gmail.com>
2022-11-06 00:43:24 +0700

    Не отправилось как будто

--- app/src/main/java/com/example/moviecatalog/viewmodel/LaunchViewModel.kt
+++ app/src/main/java/com/example/moviecatalog/viewmodel/LaunchViewModel.kt
@@ -1,28 +1,66 @@
 package com.example.moviecatalog.viewmodel
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
 import com.example.moviecatalog.R
+import com.example.moviecatalog.datastore.StoreAccessToken
 import com.example.moviecatalog.navigation.Screen
+import com.example.moviecatalog.network.Network
+import com.example.moviecatalog.network.user.UserRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+private val userRepository = UserRepository()
 
 class LaunchViewModel(private val navController: NavController) : ViewModel() {
 
-    fun navigateToSignInScreen() {
+    suspend fun tryAccessToken(context: Context, coroutineScope: CoroutineScope) {
+
+        val dataStore = StoreAccessToken(context)
+
+        coroutineScope.launch(Dispatchers.IO) {
+            dataStore.getAccessToken.collect { value ->
+                Network.token = value.toString()
+            }
+        }
+
+        coroutineScope.launch(Dispatchers.IO) {
+            userRepository.getProfile()
+                .collect { result ->
+                    result.onSuccess {
+                        launch(Dispatchers.Main) {
+                            delay(800L)
+                            navigateToMainScreen()
+                        }
+                    }.onFailure {
+                        launch(Dispatchers.Main) {
+                            delay(800L)
+                            navigateToSignInScreen()
+                        }
+                    }
+                }
+        }
+    }
+
+    private fun navigateToSignInScreen() {
         navController.popBackStack()
         navController.navigate(Screen.SignIn.route)
     }
 
-    fun navigateToSignUpScreen() {
+    private fun navigateToSignUpScreen() {
         navController.popBackStack()
         navController.navigate(Screen.SignUp.route)
     }
 
-    fun navigateToMainScreen() {
+    private fun navigateToMainScreen() {
         navController.popBackStack()
         navController.navigate(Screen.Main.route)
     }
 
-    fun navigateToMovieScreen() {
+    private fun navigateToMovieScreen() {
         navController.popBackStack()
         navController.navigate(Screen.MovieScreen.route + "/${R.drawable.featured}")
     }
